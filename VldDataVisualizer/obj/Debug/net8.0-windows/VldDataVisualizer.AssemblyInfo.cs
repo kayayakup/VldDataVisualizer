89460bcca87246a875bee6812d61a79add3b7cc8
kayayakup <55148656+kayayakup@users.noreply.github.com>
2026-08-18 09:35:39 +0300

    fix error

--- VldDataVisualizer/obj/Debug/net8.0-windows/VldDataVisualizer.AssemblyInfo.cs
+++ VldDataVisualizer/obj/Debug/net8.0-windows/VldDataVisualizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VldDataVisualizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2957d0ff02ab96ee04358c255bd7856830231c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92eccfc963f6baa12495fc4ef2df52f5cd29c8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VldDataVisualizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VldDataVisualizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
